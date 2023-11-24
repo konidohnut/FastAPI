987b6d7c91c9363e1ede2ddd3fd0c2aa53d6db0a
konidohnut <valer.novogran@gmail.com>
2023-11-24 13:13:06 +0300

    Build: Add project

---
+++ core/models/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Product",
+    "User",
+    "Post",
+
+)
+
+from .base import Base
+from .db_helper import DatabaseHelper, db_helper
+from .product import Product
+from .user import User
+from .post import Post
---
+++ main.py
@@ -0,0 +1,44 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import uvicorn
+
+from core.config import settings
+from api_v1 import router as router_v1
+from users.views import router as user_router
+from items_views import router as items_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+app.include_router(items_router)
+app.include_router(user_router)
+
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index!",
+    }
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/calc/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "result": a + b,
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
